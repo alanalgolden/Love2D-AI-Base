ebf0dcf69232504ae9348d0ade85dbb531558bcf
Alana Golden <alanalgolden@gmail.com>
2025-04-05 17:42:44 -0400

    Moved to Scripts, updated to work on folder above.

--- scripts/build.lua
+++ scripts/build.lua
@@ -9,22 +9,22 @@ local function buildLoveFile()
     local loveFile = gameName .. ".love"
     
     -- Create a temporary directory for the build
-    os.execute("mkdir -p build")
+    os.execute("mkdir -p ../build")
     
-    -- Copy all necessary files to the build directory
-    os.execute("cp -r main.lua src assets build/")
+    -- Copy all necessary files to the build directory from parent directory
+    os.execute("cp -r ../main.lua ../src ../assets ../build/")
     
     -- Remove any .git directories from the build
-    os.execute("find build -name '.git' -type d -exec rm -rf {} +")
+    os.execute("find ../build -name '.git' -type d -exec rm -rf {} +")
     
     -- Remove any .DS_Store files from the build
-    os.execute("find build -name '.DS_Store' -type f -delete")
+    os.execute("find ../build -name '.DS_Store' -type f -delete")
     
     -- Create the .love file
-    os.execute("cd build && zip -9 -r ../" .. loveFile .. " .")
+    os.execute("cd ../build && zip -9 -r ../" .. loveFile .. " .")
     
     -- Clean up the build directory
-    os.execute("rm -rf build")
+    os.execute("rm -rf ../build")
     
     print("Build complete! Created " .. loveFile)
     print("You can run the game with: love " .. loveFile)
